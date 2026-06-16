4ba3d2310a2256159d829493061c7f0f71c7ff6f
Okta010 <erlisaokta8@gmail.com>
2026-06-16 17:00:37 -0300

    Tahap 3 Lab 13

--- smartcity_citizen_spa_24782090/js/api.js
+++ smartcity_citizen_spa_24782090/js/api.js
@@ -1,5 +1,5 @@
 // js/api.js
-const BASE_URL = 'http://127.0.0.1:8000';
+const BASE_URL = 'http://103.151.63.88:8007';
 
 async function requestAPI(endpoint, method = 'GET', bodyData = null) {
     const headers = { 'Content-Type': 'application/json' };
